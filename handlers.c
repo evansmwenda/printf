01e08dc2df2bb43b570d4bdbd4bfb547866c3187
evansmwenda <evansmwenda.em@gmail.com>
2023-01-25 00:41:23 -0800

    Add style changes

--- handlers.c
+++ handlers.c
@@ -182,7 +182,8 @@ int handle_precision(va_list args, const char *modifier, char *index)
  * handle_specifiers - Matches a conversion specifier with
  *                     a corresponding conversion function.
  * @specifier: A pointer to a potential conversion specifier.
- *
+ * @char:char
+ * @int :int
  * Return: If a conversion function is matched - a pointer to the function.
  *         Otherwise - NULL.
  */
